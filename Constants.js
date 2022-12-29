90673c91598026b314156498e02a67db77aa59d5
Armaan Gupta <ohayouarmaan@gmail.com>
2022-12-29 20:25:41 +0530

    Added Constants file

---
+++ Constants.js
@@ -0,0 +1,18 @@
+const MIME_TYPES = {
+    stream: 'application/octet-stream',
+    html: 'text/html; charset=utf-8',
+    text: 'text/plain',
+    css: 'text/css',
+    js: 'text/javascript; charset=utf-8',
+    json: 'application/json; charset=utf-8',
+    png: 'image/png',
+    jpg: 'image/jpg',
+    gif: 'image/gif',
+    webp: 'image/webp',
+    ico: 'image/x-icon',
+    svg: 'image/svg+xml',
+}
+
+export {
+    MIME_TYPES
+}
