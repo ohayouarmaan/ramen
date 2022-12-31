5ac6e5683e0955a8bc54600481c4219acced9457
Armaan Gupta <ohayouarmaan@gmail.com>
2023-01-01 00:17:39 +0530

    Authentication example complete

--- examples/authentication/Routes/index.js
+++ examples/authentication/Routes/index.js
@@ -33,4 +33,13 @@ router.append("/signin", async (req, res) => {
     return res.render(path.resolve(__dirname, "..", "views", "signin.ejs"), {})
 });
 
+router.append("/test", async (req, res) => {
+    if(Object.keys(req.cookies).includes('user')) {
+        //verify token got from the client / cookie
+        if(req.cookies.user){
+            return res.send(`<h1>Hello, ${req.cookies.user}</h1>`)
+        }
+    }
+});
+
 module.exports = router;
